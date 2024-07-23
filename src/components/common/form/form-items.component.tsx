f62bceeb79c045ccc7273f4b0816ecc596e7fc94
Gmayer111 <gaelmayer@yahoo.fr>
2024-07-23 11:18:02 +0200

    feat: create form items component & form.hook

---
+++ src/components/common/form/form-items.component.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import InputForm from "./form-fields/input-form.component";
+import { TField } from "./form.hook";
+
+export type TFormItemsProps = {
+  fieldItems: TField[];
+};
+
+const FormItems = ({ fieldItems }: TFormItemsProps) => {
+  return (
+    <div>
+      {fieldItems.map((fieldItem) => {
+        const { field, inputType, placeholder, label, errorMessage } =
+          fieldItem;
+        return (
+          <div>
+            {field === "input" && (
+              <InputForm
+                name="test"
+                inputType={inputType}
+                placeholder={placeholder}
+                label={label}
+                errorMessage={errorMessage}
+              />
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FormItems;
