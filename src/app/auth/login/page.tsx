335c52341728168ad50af1a2911d58ee9209aea3
Gmayer111 <gaelmayer@yahoo.fr>
2024-07-29 09:59:35 +0200

    feat: create login page with styles

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,41 @@
+"use client";
+import Link from "next/link";
 import React from "react";
+import FormItems from "src/components/common/form/form-items.component";
+import FormRoot from "src/components/common/form/form-root.component";
+import { TField } from "src/components/common/form/form.hook";
+
+const fields: TField[] = [
+  {
+    field: "input",
+    inputType: "mail",
+    placeholder: "votre email",
+    label: "Email",
+  },
+  {
+    field: "input",
+    inputType: "password",
+    placeholder: "votre mot de passe",
+    label: "Mot de passe",
+  },
+];
 
 const Login = () => {
-  return <div>Login</div>;
+  const handleLogin = () => {
+    console.log("ok");
+  };
+  return (
+    <div className="login-container">
+      <FormRoot title="Se connecter" handleSubmit={handleLogin} isModal={false}>
+        <FormItems fieldItems={fields} />
+      </FormRoot>
+      <div className="create-login-container">
+        <p>
+          Vous n'avez pas de compte ? <Link href="#">Créer votre compte</Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
