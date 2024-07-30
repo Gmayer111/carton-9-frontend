9e0b4d92b5702712b4a5d16524282e9a3a929f5b
Gmayer111 <gaelmayer@yahoo.fr>
2024-07-30 14:54:18 +0200

    feat: replace label htmlFor by id

--- src/components/common/form/common/label-form.component.tsx
+++ src/components/common/form/common/label-form.component.tsx
@@ -2,23 +2,21 @@ import React, { forwardRef } from "react";
 
 export type TLabelProps = {
   label?: string;
-  labelFor?: string;
+  id?: string;
 };
 
 export type LabelRef = HTMLLabelElement;
 
-const LabelForm = forwardRef<LabelRef, TLabelProps>(
-  ({ label, labelFor }, ref) => {
-    if (!label) return null;
+const LabelForm = forwardRef<LabelRef, TLabelProps>(({ label, id }, ref) => {
+  if (!label) return null;
 
-    return (
-      <div>
-        <label ref={ref} className="label-form" htmlFor={labelFor}>
-          {label}
-        </label>
-      </div>
-    );
-  }
-);
+  return (
+    <div>
+      <label ref={ref} className="label-form" htmlFor={id}>
+        {label}
+      </label>
+    </div>
+  );
+});
 
 export default LabelForm;
