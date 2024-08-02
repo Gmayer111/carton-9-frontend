559e9ad2987c2baa388afaeb3ea65fb0dc448f82
Gmayer111 <gaelmayer@yahoo.fr>
2024-08-02 18:35:07 +0200

    feat: create login page up and add FormProvider

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -4,31 +4,63 @@ import React from "react";
 import FormItems from "src/components/common/form/form-items.component";
 import FormRoot from "src/components/common/form/form-root.component";
 import { TField } from "src/components/common/form/form.hook";
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
 
 const fields: TField[] = [
   {
     field: "input",
-    inputType: "mail",
     placeholder: "votre email",
     label: "Email",
+    name: "email",
+    registerOptions: {
+      required: true,
+      requiredMessage: "Email obligatoire",
+    },
   },
   {
     field: "input",
     inputType: "password",
     placeholder: "votre mot de passe",
     label: "Mot de passe",
+    name: "password",
+    registerOptions: {
+      required: true,
+      requiredMessage: "Mot de passe obligatoire",
+    },
   },
 ];
 
+export type TLoginFormValues = {
+  email: string;
+  password: string;
+};
+
 const Login = () => {
-  const handleLogin = () => {
-    console.log("ok");
+  const methods = useForm<TLoginFormValues | FieldValues>();
+  const {} = methods;
+
+  const handleLogin: SubmitHandler<TLoginFormValues | FieldValues> = async (
+    data
+  ) => {
+    console.log(data);
   };
   return (
     <div className="login-container">
-      <FormRoot title="Se connecter" handleSubmit={handleLogin} isModal={false}>
-        <FormItems fieldItems={fields} />
-      </FormRoot>
+      <FormProvider {...methods}>
+        <FormRoot
+          title="Se connecter"
+          methods={methods}
+          onSubmit={handleLogin}
+          isModal={false}
+        >
+          <FormItems fieldItems={fields} />
+        </FormRoot>
+      </FormProvider>
       <div className="create-login-container">
         <p>
           Vous n'avez pas de compte ? <Link href="#">Créer votre compte</Link>
