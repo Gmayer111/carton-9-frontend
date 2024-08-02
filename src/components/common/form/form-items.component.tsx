7404794e8f539f7e94e1b93877aa56ab0602a777
Gmayer111 <gaelmayer@yahoo.fr>
2024-08-02 18:37:23 +0200

    feat: update form-item/root components about react-form

--- src/components/common/form/form-items.component.tsx
+++ src/components/common/form/form-items.component.tsx
@@ -1,26 +1,44 @@
 import React from "react";
 import InputForm from "./form-fields/input/input-form.component";
-import { TField } from "./form.hook";
+import { TField } from "./form";
+import { useFormContext } from "react-hook-form";
 
 export type TFormItemsProps = {
   fieldItems: TField[];
 };
 
 const FormItems = ({ fieldItems }: TFormItemsProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
   return (
     <div>
       {fieldItems.map((fieldItem) => {
-        const { field, inputType, placeholder, label, errorMessage } =
+        const { field, inputType, placeholder, label, name, registerOptions } =
           fieldItem;
+
         return (
           <div>
             {field === "input" && (
               <InputForm
-                name="test"
                 inputType={inputType}
                 placeholder={placeholder}
                 label={label}
-                errorMessage={errorMessage}
+                errorMessage={
+                  errors[name]?.message && (errors[name]?.message as string)
+                }
+                {...register(name, {
+                  required: {
+                    value: registerOptions?.required as boolean,
+                    message: registerOptions?.requiredMessage as string,
+                  },
+                  pattern: {
+                    value: registerOptions?.regexPattern as RegExp,
+                    message: registerOptions?.regexPatternMessage as string,
+                  },
+                })}
               />
             )}
           </div>
