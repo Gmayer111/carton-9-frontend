6edec4364a474c922b5b612eb6e532eb202a4bd9
Gmayer111 <gaelmayer@yahoo.fr>
2024-08-22 16:37:25 +0200

    feat: create user type

---
+++ src/types/user.ts
@@ -0,0 +1,10 @@
+export type TUser = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  birthdate?: Date;
+  description?: string;
+  nationality?: string;
+  picture?: string;
+};
