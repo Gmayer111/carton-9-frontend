9d7169b29b88404ec706a68f801b704be7de736f
Gmayer111 <gaelmayer@yahoo.fr>
2024-09-13 19:17:51 +0200

    feat: create cancel modal

--- src/components/common/modal/modal.component.tsx
+++ src/components/common/modal/modal.component.tsx
@@ -1,27 +1,43 @@
 import React, { ReactNode, useRef } from "react";
 import ButtonForm from "../form/form-fields/button/button-form.component";
 import { useClickOutside } from "src/hooks/common.hook";
-import { XMarkIcon } from "@heroicons/react/24/solid";
+import { TrashIcon, XMarkIcon } from "@heroicons/react/24/solid";
 
 export type TModalProps = {
   children: ReactNode;
   closeModal: () => void;
   modalHeaderTitle?: string;
+  isCancelModal?: boolean;
 };
 
-const Modal = ({ children, closeModal, modalHeaderTitle }: TModalProps) => {
+const Modal = ({
+  children,
+  closeModal,
+  modalHeaderTitle,
+  isCancelModal,
+}: TModalProps) => {
   const modalRef = useRef(null);
 
   useClickOutside({ ref: modalRef, callback: closeModal });
   return (
     <div className="modal-container">
       <div>
-        <div ref={modalRef} className="modal-content-container">
+        <div
+          ref={modalRef}
+          className={`${
+            isCancelModal && "cancel-modal-container"
+          } modal-content-container`}
+        >
           <div className="modal-header-container">
             <h3>{modalHeaderTitle}</h3>
             <ButtonForm hasIconButton={true} onClick={closeModal}>
               <XMarkIcon />
             </ButtonForm>
+            {isCancelModal && (
+              <div className="trashIcon-modal-container">
+                <TrashIcon />
+              </div>
+            )}
           </div>
           {children}
         </div>
