bc2313e51a3044b8935a03a5ce1691db533a125a
Gmayer111 <gaelmayer@yahoo.fr>
2024-09-16 10:37:52 +0200

    feat: add button colors

--- src/components/common/form/form-fields/button/button-form.component.tsx
+++ src/components/common/form/form-fields/button/button-form.component.tsx
@@ -2,17 +2,32 @@ import React from "react";
 import { TButtonForm } from "../../form";
 
 const ButtonForm = React.forwardRef<HTMLButtonElement, TButtonForm>(
-  ({ isLoading, children, onClick, type, isDisabled, hasIconButton }, ref) => {
+  (
+    {
+      isLoading,
+      children,
+      onClick,
+      type,
+      isDisabled,
+      hasIconButton,
+      colorButton,
+    },
+    ref
+  ) => {
     return (
-      <button
-        className={hasIconButton ? "dropwdown-icon-button" : "button-form"}
-        onClick={onClick}
-        ref={ref}
-        type={type}
-        disabled={isLoading || isDisabled}
-      >
-        {children}
-      </button>
+      <div className="button-container">
+        <button
+          className={`${
+            hasIconButton ? "dropwdown-icon-button" : "button-form"
+          } ${colorButton}`}
+          onClick={onClick}
+          ref={ref}
+          type={type}
+          disabled={isLoading || isDisabled}
+        >
+          {children}
+        </button>
+      </div>
     );
   }
 );
