7f5f376dc6e7d89b21f793eb3c054163f5cd7275
Gmayer111 <gaelmayer@yahoo.fr>
2024-09-20 19:40:37 +0200

    fix: refacto create & edit modal

--- src/components/common/modal/create-modal.component.tsx
+++ src/components/common/modal/create-modal.component.tsx
@@ -9,6 +9,7 @@ import {
 import FormRoot from "../form/form-root.component";
 import FormItems from "../form/form-items.component";
 import { TFields } from "../form/form";
+import ButtonForm from "../form/form-fields/button/button-form.component";
 
 export type TCreateModalProps = Pick<
   TModalProps,
@@ -18,6 +19,7 @@ export type TCreateModalProps = Pick<
   displayModal: boolean;
   formMethods: UseFormReturn<FieldValues, any, undefined>;
   handleSubmit: SubmitHandler<FieldValues>;
+  isEditModal?: boolean;
 };
 
 const CreateModal = ({
@@ -27,6 +29,7 @@ const CreateModal = ({
   formMethods,
   handleSubmit,
   modalHeaderTitle,
+  isEditModal,
 }: TCreateModalProps) => {
   if (!displayModal) return null;
 
@@ -36,10 +39,22 @@ const CreateModal = ({
         <FormRoot
           methods={formMethods}
           onSubmit={handleSubmit}
-          hasCloseButton={true}
           handleCloseActionButton={closeModal}
+          isModalForm={true}
         >
           <FormItems fieldItems={modalFields} />
+          <div className="buttons-action-modal-container">
+            <ButtonForm
+              type={"submit"}
+              children={isEditModal ? "Modifier" : "Ajouter"}
+            />
+            <ButtonForm
+              type={"button"}
+              children="Annuler"
+              onClick={closeModal}
+              colorButton="alert"
+            />
+          </div>
         </FormRoot>
       </FormProvider>
     </Modal>
